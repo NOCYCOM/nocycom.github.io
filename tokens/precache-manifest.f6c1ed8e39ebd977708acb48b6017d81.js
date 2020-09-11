0a136cd805f4c10adca030505a542df413251452
xiao198304 <386590727@qq.com>
2020-09-11 09:57:58 +0800

    deploy

---
+++ tokens/precache-manifest.f6c1ed8e39ebd977708acb48b6017d81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c5a6768354f543e588f821088f0a8d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4522472386db726a1ff0",
+    "url": "./static/css/2.0fe736ec.chunk.css"
+  },
+  {
+    "revision": "dae8a90da78d362cb5a8",
+    "url": "./static/css/main.1fa3f329.chunk.css"
+  },
+  {
+    "revision": "4522472386db726a1ff0",
+    "url": "./static/js/2.0144937e.chunk.js"
+  },
+  {
+    "revision": "7dd5a6d3b0317d9672d5b7d11992b67b",
+    "url": "./static/js/2.0144937e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dae8a90da78d362cb5a8",
+    "url": "./static/js/main.24c296ef.chunk.js"
+  },
+  {
+    "revision": "3535b526546af2db71cb",
+    "url": "./static/js/runtime-main.57b00fa4.js"
+  }
+]);
